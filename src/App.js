aa1c29d61b670bf16f74b00ecf9326051dc10d9f
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-08 14:04:40 +0100

    create ripository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useReducer , useEffect } from 'react';
+
+function Header(props){
+  // props is object  
+  // use state 
+  const [states, setStates] = useState("happy");
+  const [checked, toggle] = useReducer(
+    (checked) => !checked
+    ,false
+    );
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    <h2>{props.esiName}</h2>
+    <h2>state now : {states} .</h2>
+    <button onClick = {()=> setStates("bad")}>Click Bad</button>
+    <button onClick = {()=> setStates("happy")}>Click Happy</button>
+    <input type="checkbox" value={checked} onChange={toggle}></input>
+    {(checked)?<h1>Check</h1>:<h1>uncheck</h1>}
+    </>
+  );
+}
+
+const dishes = [
+  "Apple","Banana","Ananas","Mongue"
+];
+
+let dishesObj = dishes.map((d,i)=>({id:i,name:d}));
+
+function Body(p){
+  return <> 
+  <h3> this is a body </h3> 
+  <ul style = {{textAlign:"left",background:"red",color:"blue"}}>
+    {p.dishes.map((d,index)=>(<li key={index}>{d}</li>))}
+  </ul>
+  </>
+}
+function Other(p){
+  return (
+    <ul style = {{textAlign:"center",background:"yellow",color:"green"}}>
+    {p.dishes.map((d)=>(<li key={d.id}>{d.name}</li>))}
+    </ul>
+  );
+}
+
+function fetchUser(){
+
+  const [data, setData ] = useState(null);
+
+  useEffect(() => {
+     fetch(`https://api.github.com/users/wycats`)
+     .then( (response) => response.json())
+     .then(setData);
+  },[]);
+
+  
+  if(data){
+    return <div>{JSON.stringify(data)}</div>;
+  }
+  return <div> NO Data</div>;
+  
+
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [data, setData ] = useState(null);
+
+  useEffect(() => {
+     fetch(`https://api.github.com/users/wycats`)
+     .then( (response) => response.json())
+     .then(setData);
+  },[]);
+
+  
+  if(data){
+    return (<div>{JSON.stringify(data)}</div>);
+  }
+  return (<div> NO Data</div>);
+  
+
+  // return (
+  //   <div className="App">
+  //     <fetchUser />
+  //     {/* <Header esiName="ESI Alger"/>
+  //     <Body dishes = {dishes}/>
+  //     <Other dishes = {dishesObj}/>
+  //     <img src = {logo} height = {200} alt="img"></img> */}
+  //   </div>
+  // );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//React.createElement("h1",{style:{color:"blue"}},"hello world!!!")
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
+/* import App function from file and put it in tag <App/> */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* ReactDOM.render(Element , whenPutElement );
+to create element React.createElement("tag",{style,classname ..}, "enter the tag");
+
+*/
